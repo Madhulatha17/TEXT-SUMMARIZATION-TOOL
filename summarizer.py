6c5acdbd5527b31a1a2d7438bf942341e9cc08c4
Madhulatha17 <bandimadhulatha4@gmail.com>
2026-06-10 17:00:39 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,16 +1,12 @@
 from collections import Counter
 
-# Get input text from user
 text = input("Enter a paragraph:\n")
 
-# Split text into sentences
 sentences = text.split('.')
 
-# Count word frequencies
 words = text.lower().split()
 word_freq = Counter(words)
 
-# Score each sentence
 scores = {}
 
 for sentence in sentences:
@@ -19,17 +15,14 @@ for sentence in sentences:
         score += word_freq[word]
     scores[sentence] = score
 
-# Select top 2 important sentences
 summary_sentences = sorted(
     scores,
     key=scores.get,
     reverse=True
 )[:2]
 
-# Generate summary
 summary = ". ".join(summary_sentences)
 
-# Display results
 print("\n----- ORIGINAL TEXT -----")
 print(text)
 
